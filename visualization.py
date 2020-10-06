55841a255d40c60059082172fc2c238a3ccf438c
niklasn <niklasneugebauer@outlook.com>
2020-10-06 14:07:12 +0200

    remove incorrect "gl."

--- visualization.py
+++ visualization.py
@@ -2,6 +2,9 @@ import numpy as np
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import OpenGL.arrays.vbo as glvbo
+
+
 
 def square(position, color):
         side_len = 20
@@ -19,23 +22,28 @@ class KurvenPlotter:
     def __init__(self, width, height):
         self.width, self.height = width, height
         glViewport(0, 0, width, height)
-        glMatrixMode(gl.GL_PROJECTION)
+        glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(-1, 1, 1, -1, -1, 1)
+        glClearColor(0,0,0,0)
 
     def load_positions(self, data):
         """ Load new curve positions as a numpy array
         """
         self.data = data
         self.count = data.shape[0]
+        self.vbo = glvbo.VBO(self.data)
 
-    def paintCurrent(self):
+    def paintCurrent(self, clear):
         """ Paint a dot at all current positions
         """
+        if clear :
+            glClear(GL_COLOR_BUFFER_BIT)
+        glColor(1,1,0)
         self.vbo.bind()
-        glEnableClientState(gl.GL_VERTEX_ARRAY)
-        glVertexPointer(2, gl.GL_FLOAT, 0, self.vbo)
-        glDrawArrays(gl.GL_POINTS, 0, self.count)
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glVertexPointer(2, GL_FLOAT, 0, self.vbo)
+        glDrawArrays(GL_POINTS, 0, self.count)
         # TODO is there a way to set vertex colors efficiently?
 
     def getCurrentImage(self):
