557733f834595eebd524d7051d3ca7598863b9ce
niklasn <niklasneugebauer@outlook.com>
2020-10-09 23:50:53 +0200

    more experiments and file export

--- scene.py
+++ scene.py
@@ -2,22 +2,40 @@ import pygame as pg
 from pygame.locals import *
 import numpy as np
 import numpy.random
+import itertools
+
+from PIL import Image
+from PIL import ImageOps
 
 from visualization import KurvenPlotter, PygamePlotter
 from kurve import Kurve
 
+<<<<<<< Updated upstream
 numKurven = 2000
 speed = .1
 window = (1000, 1000)
 foresight = 20
+=======
+record = True
+live = True
+record_stride = 50
+
+numKurven = 1000
+speed = .1
+window = (1000, 1000)
+foresight = 10
+
+>>>>>>> Stashed changes
 
 def position_array(kurven):
     positions = []
     for k in kurven:
-        positions.append(k.get_pos())
+        x = k.get_pos()
+        positions.append((2 * x[0]/window[0] - 1., 2 * x[1]/window[1] - 1.))
     
     return np.array(positions)
 
+<<<<<<< Updated upstream
 def not_black(color):
     return color[0] != 0 or color[1] != 0 or color[2] != 0
 
@@ -50,8 +68,47 @@ def steer(k, display, dt):
 pg.init()
 
 display = pg.display.set_mode(window)
+=======
+def wiggle(k):
+    k.rotate(3.1415/np.random.randint(1, 5))
 
-plotter = PygamePlotter(display, numKurven)
+def not_black(color):
+    return color[0] != 0 or color[1] != 0 or color[2] != 0
+
+def oob(koord):
+    return koord[0] >= window[0] or koord[0] < 0 or koord[1] >= window[1] or koord[1] < 0
+
+def steer(k, image, dt):
+    turn = False
+    pos = k.get_pos()
+    vel = k.get_vel()
+    rot = k.get_rot()
+    
+    for i in range(int(vel * foresight * dt)):
+        koord = ( int(pos[0] + i * np.sin(rot)), int(pos[1] + i * np.cos(rot)) )
+        if oob(koord):
+            turn = True
+            break
+        color = image.getpixel(koord)
+        if not_black(color):
+            turn = True
+            break
+
+    if turn:
+        dir = np.random.random_integers(0, 2)
+        if dir == 0:
+            k.rotate(.2)
+>>>>>>> Stashed changes
+
+#def fill(k, display):
+#    for x, y in itertools.product([-1, 0, 1], [-1, 0, 1]):
+        
+if live:
+    pg.init()
+
+    display = pg.display.set_mode(window, pg.locals.OPENGL)
+
+plotter = KurvenPlotter(window[0], window[1], numKurven)
 kurven = []
 for i in range(numKurven):
     posi = (np.random.randint(0, window[0]), np.random.randint(0, window[1]))
@@ -61,19 +118,33 @@ for i in range(numKurven):
 
 plotter.update_positions(position_array(kurven), np.arange(numKurven))
 
+<<<<<<< Updated upstream
 cols = np.array(list(zip(255 * .5 * (np.sin(np.arange(numKurven)) + np.ones(numKurven)), 255 * .5 * (np.sin(1. + np.arange(numKurven)) + np.ones(numKurven)), 255 * .5 * (np.sin(2. + np.arange(numKurven)) + np.ones(numKurven)), 255 * np.ones(numKurven))))
 #cols = np.array(list(zip(255 * np.ones(numKurven), 255 * np.ones(numKurven), 255 * np.ones(numKurven), 255 * np.ones(numKurven))))
+=======
+cols = np.array(list(zip(.5 * (np.sin(3.1415 * np.arange(numKurven)/numKurven) + np.ones(numKurven)),.5 * (np.sin(1. + 3.1415 * np.arange(numKurven)/numKurven) + np.ones(numKurven)), .5 * (np.sin(2. + 3.1415 * np.arange(numKurven)/numKurven) + np.ones(numKurven)), np.ones(numKurven))))
+>>>>>>> Stashed changes
 plotter.update_colors(cols)
 count = 0
 
+count = 0
 while True:
-    for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                quit()
+    if live:
+        for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    quit()
+
+    data = plotter.getCurrentImage()
+    image = Image.frombytes("RGBA", (window[0], window[1]), data)
+    #image = ImageOps.flip(image) # in my case image is flipped top-bottom for some reason
+
+    if record and count % record_stride == 0:
+        image.save('export/' + str(int(count/record_stride)) + '.png', 'PNG')
 
     for k in kurven:
         k.move(10)
+<<<<<<< Updated upstream
         steer(k, display, 10)
         
     if count % 200 < 15:
@@ -87,4 +158,15 @@ while True:
     pg.display.flip()
     pg.time.wait(10)
     count += 1
+=======
+        wiggle(k)
+        
+    plotter.update_positions(position_array(kurven), np.array([]))
+    plotter.paint(False)
+>>>>>>> Stashed changes
 
+    if live:
+        pg.display.flip()
+
+    count += 1
+    
--- visualization.py
+++ visualization.py
@@ -2,13 +2,10 @@ import numpy as np
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import OpenGL.arrays.vbo as vbo
+import OpenGL.arrays.vbo as glvbo
 
 import pygame as pg
 from pygame.locals import *
-
-
-
 
 def square(position, color):
         side_len = .1
@@ -73,12 +70,16 @@ class KurvenPlotter:
     def __init__(self, width, height, count):
         self.width, self.height = width, height
         self.count = count
-        self.positions = np.array(list(zip(np.zeros(count), np.zeros(count))), dtype=np.float32)
-        #self.update_positions(self.positions, [])
+        self.positions = np.array(list(zip(np.zeros(count), np.zeros(count))))
+        self.update_positions(self.positions, [])
         self.colors = np.array(list(zip(np.ones(count), np.ones(count), np.ones(count), np.ones(count))))
-        #self.update_colors(self.colors)
-
+        self.update_colors(self.colors)
         glViewport(0, 0, width, height)
+        glClearColor(0,0,0,0)
+        # setup OpenGL to work with arrays instead of individual primitive commands
+        glEnableClientState(GL_VERTEX_ARRAY)
+        # setup OpenGL to work with arrays instead of individual color commands
+        glEnableClientState(GL_COLOR_ARRAY)
 
     def update_positions(self, data, reset):
         """ Load new curve positions as a numpy array of format [x, y]
@@ -87,14 +88,14 @@ class KurvenPlotter:
         """
         # copy last positions
         last_positions = self.positions.copy()
-        self.positions = np.require(data.copy(),np.float32,'F')
+        self.positions = data.copy()
         # reset lines where requested by placing its start at the new position, too
         for i in reset:
             last_positions[i] = data[i]
         
         # interlace positions with last_positions (format used by opengl is [start1, end1, start2, end2, ...])
-        lines = np.array([number for pair in zip(last_positions, self.positions) for val in pair for number in val], dtype=np.float32)
-        self.pos_buf.set_array(lines)
+        lines = np.array([val for pair in zip(last_positions, self.positions) for val in pair], dtype=np.float32)
+        self.pos_buf = glvbo.VBO(lines)
 
     def update_colors(self, data):
         """ load new colors as a full numpy array of format [[r, g, b, a]]
@@ -103,7 +104,7 @@ class KurvenPlotter:
 
         # interlace colors with their old counterparts as there will be 2 vertices per line
         pairs = np.array([val for pair in zip(self.colors, self.colors) for val in pair], dtype=np.float32)
-        #self.col_buf = glvbo.VBO(pairs)
+        self.col_buf = glvbo.VBO(pairs)
 
     def paint(self, clear):
         """ Paint a line for all data points
@@ -111,26 +112,21 @@ class KurvenPlotter:
         if clear :
             glClear(GL_COLOR_BUFFER_BIT)
 
-        #self.col_buf.bind()
-        #try:
-            # mark col_buf as specifying vertex_colors
-        #    glColorPointer(4, GL_FLOAT, 0, self.col_buf)
-        #finally:
-            # unbind the buffer to make room for positions
-        #    self.col_buf.unbind()
 
-        # setup OpenGL to work with arrays instead of individual primitive commands
-        glEnableClientState(GL_VERTEX_ARRAY)
+        self.col_buf.bind()
+        # mark col_buf as specifying vertex_colors
+        glColorPointer(4, GL_FLOAT, 0, self.col_buf)
+        # unbind the buffer to make room for positions
+        self.col_buf.unbind()
+
         self.pos_buf.bind()
-        
         # mark pos_buf as a specifying vertices
         glVertexPointer(2, GL_FLOAT, 0, self.pos_buf)
-
-        glDrawArrays(GL_LINES, 0, 2 * self.count)
-        glDisableClientState(GL_VERTEX_ARRAY)
+        # unbin the buffer just in case
         self.pos_buf.unbind()
-        square([0,0], [1.,1.,1.,1.])
+        
+        glDrawArrays(GL_LINES, 0, 2 * self.count)
 
     def getCurrentImage(self):
-        # TODO can we use pixel buffer objects for this?
-        print("unimplemented!")
+        glPixelStorei(GL_PACK_ALIGNMENT, 1)
+        return glReadPixels(0, 0, self.width, self.height, GL_RGBA, GL_UNSIGNED_BYTE)
